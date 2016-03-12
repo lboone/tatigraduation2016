37e606c31ee8048b55c92b2d6bcfa7ab57c747e1
Melissa Boone <mlboone3@me.com>
2016-03-11 23:49:07 -0500

    Initial Commit

---
+++ tatigraduation2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tatigraduation2016
+//
+//  Created by Lloyd Boone on 3/11/16.
+//  Copyright © 2016 LAMB Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
